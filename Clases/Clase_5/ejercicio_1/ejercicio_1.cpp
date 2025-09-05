56ef23c52d314a5b6310fb9b97761bd3510c6817
ggonzaDNG <gonzalodeangelis641@gmail.com>
2025-09-05 11:32:40 -0300

    Tarea

--- Clases/Clase_5/ejercicio_1/ejercicio_1.cpp
+++ Clases/Clase_5/ejercicio_1/ejercicio_1.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+/*
+ Escribir un programa que lea del archivo "ejercicio1in.txt" tres números enteros. 
+ El programa debe generar un archivo "ejercicio1out.txt" con los tres números ordenados de menor a mayor.
+*/
+
 int main() {
-    struct persona {
-        string nombre;
-        string apellido;
-        char letra_fav;
-        int edad;
-        int meses;
-        int dias;
+    int num;
+
+    ifstream file("ejercicio1in.txt");
+    ofstream fileOut("ejercicio1out.txt");
+    string str;
+    vector <int> numeros;
+
+    while (file >> str) {
+        istringstream iss(str);
+
+        while (iss >> num) {
+            numeros.push_back(num);
+        }
+
+        sort(numeros.begin(), numeros.end()); // sorteo de menor a mayor
     }
 
-    ifstream infile("/persona.txt");
-    for (string linea; getline(infile, linea)) {
-        cout << linea;
+    for (int j : numeros) {
+            cout << j << ' '; // imprimo en la consola 
     }
     
+    for (int k = 0; k < numeros.size(); k++) {
+        fileOut << numeros[k] << ' '; // escribo al archivo de salida
+    }
+
     return 0;
 
 }
